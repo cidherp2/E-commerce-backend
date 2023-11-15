1cb5891a36b13a133d60e110ed0735ff762025c1
Alejandro Larios <alexlarios70@hotmail.com>
2023-11-15 05:07:36 -0600

    to get the commits to show

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<style>h1{text-align:center; font-size:xxx-large;} body{display:flex; justify-content:center; height:100vh; width:100%; align-items: center; overflow: hidden;} </style><h1>Hey there hot stuff ;)</h1>")
 });
 
 module.exports = router;
